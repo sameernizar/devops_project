a921e782e9f82367d8abed2e89a81f7497a5876b
sameer <sameernizar67@gmail.com>
2025-06-26 23:19:17 +0500

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,14 @@
-  provider "azurerm" {
-    features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
   }
+}
+provider "azurerm" {
+  features {}
+}
 
   resource "azurerm_resource_group" "rg" {
     name     = var.resource_group
